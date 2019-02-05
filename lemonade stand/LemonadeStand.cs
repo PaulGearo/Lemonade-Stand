3dcfe86b6d9efb9733def56d825c25eff27f2e60
Paul Gearo <PaulGearo@gmail.com>
2019-02-05 08:33:44 -0600

    finished body of program, and added methods. working program

---
+++ lemonade stand/LemonadeStand.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lemonade_stand
+{
+    class LemonadeStand
+    {
+        public LemonadeStand()
+        {
+            Name = Program.GetStringResponce("What's the name of the Stand?");
+            MonthsInOperation = Program.GetIntResponce("How may months were you open?");
+            NumberOfCupsSoldPerMonth = Program.GetIntResponce("How may cups did you sell?");
+            PricePerCup = Program.GetDoubleResponce("what was the price per cup?");
+            LemonsCostPerCup = Program.GetDoubleResponce("What was the cost of lemons per cup?");
+            SugerCostPerCup = Program.GetDoubleResponce("what was the cost of suger per cup?");
+            PaperCup = Program.GetDoubleResponce("whats the cost of the paper cups sold?");
+            TableCost = Program.GetDoubleResponce("What was the cost of table");
+            ChairCost = Program.GetDoubleResponce("what was the cost of chair");
+            SignCost = Program.GetDoubleResponce("what was the cost of sign?");
+        }
+
+        public string Name { get; set; }
+        public int MonthsInOperation { private get; set; }
+        public int NumberOfCupsSoldPerMonth { private get; set; }
+        public double PricePerCup { private get; set; }
+        public double SugerCostPerCup { private get; set; }
+        public double LemonsCostPerCup { private get; set; }
+        public double PaperCup { private get; set; }
+        public double ChairCost { private get; set; }
+        public double TableCost { private get; set; }
+        public double SignCost { private get; set; }
+
+        private double GetRevenueForMonth()
+        {
+            return PricePerCup * NumberOfCupsSoldPerMonth;
+        }
+        private double GetExpensesForMonthExucldingOverhead()
+        {
+            return NumberOfCupsSoldPerMonth * (SugerCostPerCup + LemonsCostPerCup + PaperCup);
+        }
+        private double GetProfitForMonthExludingOverhead()
+        {
+            return GetRevenueForMonth() - GetExpensesForMonthExucldingOverhead();
+        }
+        private double GetOverheadCosts()
+        {
+            return ChairCost + TableCost + SignCost;
+        }
+
+        private double GetLifeTimeRevenue()
+        {
+            return (GetRevenueForMonth() * MonthsInOperation);
+        }
+
+        private  double GetLifetimeMonthlyProfits()
+        {
+            return GetExpensesForMonthExucldingOverhead() * MonthsInOperation;
+        }
+
+        public double GetLifetimeTotalProfit()
+        {
+            return GetLifeTimeRevenue() - GetOverheadCosts();
+        }
+    }
+}
--- lemonade stand/Program.cs
+++ lemonade stand/Program.cs
@@ -4,9 +4,61 @@ namespace lemonade_stand
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int NumberOFstands = GetIntResponce("how may will you open?");
+
+            double empireNetProfit = 0;
+            for (int i = 0; i < NumberOFstands; i++)
+            {
+            LemonadeStand lemonadeStand = new LemonadeStand();
+            double netProfit = lemonadeStand.GetLifetimeTotalProfit();
+                empireNetProfit += netProfit;
+            Console.WriteLine($"Ok, so the total profit {lemonadeStand.Name} " +
+                $"Stand will ${netProfit}");
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+            Console.WriteLine("your empire will have a " +
+                $"net profit if ${empireNetProfit}.");
+
+
+            Console.ReadLine();
         }
+
+        public static int GetIntResponce(string question)
+        {
+            Console.WriteLine(question);
+            string responseStr = Console.ReadLine();
+            int response;
+            while (int.TryParse(responseStr, out response) == false)
+            {
+                Console.WriteLine("please enter an integer");
+                responseStr = Console.ReadLine();
+            }    
+            return response;
+        }
+
+        public static double GetDoubleResponce(string question)
+        {
+            Console.WriteLine(question);
+            string responseStr = Console.ReadLine();
+            double response;
+            while (double.TryParse(responseStr, out response) == false)
+            {
+                Console.WriteLine("please enter an number");
+                responseStr = Console.ReadLine();
+            }
+            return response;
+        }
+        public static string GetStringResponce(string question)
+        {
+            Console.WriteLine(question);
+            string response = Console.ReadLine();
+            return response;
+        }
+
+
     }
 }
